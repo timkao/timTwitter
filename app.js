eba029ef8ead6c93d740930d76d7fbee2d3d5be9
Tim Kao <pakuya.kao@gmail.com>
2017-07-12 21:13:16 -0400

    setup ready

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const chalk = require('chalk');
+const volleyball = require('volleyball');
 const app = express();
+
 
 var port = process.env.PORT || 3000;
 
+
+app.use(volleyball);
+
+/*
+app.use(function(req, res, next) {
+  console.log(chalk.blue(req.method + ' ' + req.url + ' ' + res.statusCode));
+  next();
+});
+*/
+
+app.use('/special/', function(req, res, next){
+  console.log('special area');
+});
 
 app.get('/', function(req, res){
   res.send('Hello!');
