5a4f4d87356fe9da0c13f81997ed4b2cce1b0479
Tim Kao <pakuya.kao@gmail.com>
2017-07-15 10:54:14 -0400

    all done

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
 const chalk = require('chalk');
 const volleyball = require('volleyball');
+const routes = require('./routes');
 const app = express();
-
-
+const bodyParser = require('body-parser');
+const socketio = require('socket.io');
 var port = process.env.PORT || 3000;
 
+var server = app.listen(port);
+var io = socketio.listen(server);
 
+
+// alwasy search html files
+app.set('view engine', 'html');
+// i want html rendered by nunjucks.render
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', {noCache: true});
 app.use(volleyball);
+app.use(express.static('public'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
-/*
-app.use(function(req, res, next) {
-  console.log(chalk.blue(req.method + ' ' + req.url + ' ' + res.statusCode));
-  next();
-});
-*/
+app.use('/', routes(io));
 
-app.use('/special/', function(req, res, next){
-  console.log('special area');
-});
-
-app.get('/', function(req, res){
-  res.send('Hello!');
-});
-
-app.listen(port, function() {
-  console.log(`listening on port ${port}`);
-});
