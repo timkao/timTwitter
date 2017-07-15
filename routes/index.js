12c085a414f491c430129d562e6de27bba33aae7
Tim Kao <pakuya.kao@gmail.com>
2017-07-15 11:03:42 -0400

    delete unneccessary code

--- routes/index.js
+++ routes/index.js
@@ -2,32 +2,6 @@ const express = require('express');
 const router = express.Router();
 // could use one line instead: const router = require('express').Router();
 const tweetBank = require('../tweetBank');
-/*
-router.get('/', function (req, res) {
-  let tweets = tweetBank.list();
-  res.render( 'index', { tweets: tweets, showForm: true } );
-  console.log(tweets);
-});
-
-router.get('/users/:name', function(req, res){
-  var name = req.params.name;
-  var list = tweetBank.find({name: name});
-  res.render('index', {tweets: list, showForm: true, person: name});
-});
-
-router.get('/tweets/:id', function(req, res){
-  var id = req.params.id;
-  var list = tweetBank.find({id: Number(id)});
-  res.render('index', {tweets: list});
-});
-
-router.post('/tweets', function(req, res){
-  var name = req.body.name;
-  var content = req.body.text;
-  tweetBank.add(name, content);
-  res.redirect('/');
-});
-*/
 
 module.exports = function(io) {
 
